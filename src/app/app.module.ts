b3e0b411ff50a3efea5cab71edac6b96a2053d58
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-05-04 18:00:00 -0500

    Fin Sección 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { app_routing } from "./app.routes";
+
+//Componentes
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioItemComponent } from './components/portfolio-item/portfolio-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent,
+    PortfolioItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes} from '@angular/router';
+
+import { 
+    AboutComponent,
+    PortfolioComponent,
+    PortfolioItemComponent 
+} from './components/index.paginas';
+
+const app_routes: Routes = [
+    { path: 'home', component: PortfolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: PortfolioItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+
+];
+
+export const app_routing =RouterModule.forRoot(app_routes, {useHash: true});
