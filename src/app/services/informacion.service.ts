5b1c07959afd33cdd8eb1627feda7068f1c1b543
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-05-07 12:03:08 -0500

    Fin Sección 5

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,30 @@ export class InformacionService {
 
   info : any ={};
   cargada : boolean = false;
+  cargada_sobre_nosostros : boolean = false;
+  equipo: any[] = [];
 
   constructor(public http:Http) { 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
 
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
           .subscribe ( data =>{
-            console.log(data);
+            //console.log(data);
             this.info = data.json();
             this.cargada= true;
           })
   }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://paginaweb-9d7e1.firebaseio.com/equipo.json")
+          .subscribe ( data =>{
+            console.log(data);
+            this.equipo = data.json();
+            this.cargada_sobre_nosostros= true;
+          })
+  }
+
 }
